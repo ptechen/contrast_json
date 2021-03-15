90f62f72e6fe2b165575bf804a3832ca5ee79b1d
陶辰 <taochen@taochendeMacBook-Pro-2.local>
2021-03-15 10:39:32 +0800

    update

--- render_compare/json_compare_render_test.go
+++ render_compare/json_compare_render_test.go
@@ -8,7 +8,9 @@ import (
 func TestJsonCompareRender(t *testing.T) {
 	json1 := map[string]interface{}{"test": "123", "rrr": 0, "rrreew": map[string]interface{}{"432": "234", "321": "432"}}
 	json2 := map[string]interface{}{"test": "321", "rrreew": map[string]interface{}{"432": "234"}, "dfass": []string{"ewq"}}
-	JsonCompareRender(json1, json2)
+	res, ok := JsonCompareRender(json1, json2, -1)
+	fmt.Println(ok)
+	fmt.Println(res)
 	fmt.Println(json1)
 	fmt.Println(json2)
 }
